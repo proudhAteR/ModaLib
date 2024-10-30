38fe636bd5942e6fca5abc456d9da6f045968961
proudhAteR <158060444+proudhAteR@users.noreply.github.com>
2024-10-30 02:08:12 -0400

    feature : basic design for custom, alert and confirm modals

--- Public/javascripts/main.js
+++ Public/javascripts/main.js
@@ -1,13 +1,45 @@
-import { searchModal } from "./modals.js";
+import { searchModal as findModal, generateModal, hide, show, } from "./modals.js";
 let triggerButton;
-export const modals = Array.from(document.querySelectorAll(".modal-container"));
-document.addEventListener("click", (e) => {
-    const target = e.target;
+let triggeredElement;
+document.body.addEventListener("click", (e) => {
+    let target = e.target;
+    if (target.closest("[data-alert]")) {
+        defaultModals(target);
+    }
+    if (target.closest("[data-confirm]")) {
+        defaultModals(target);
+    }
     if (target.closest("[data-trigger]")) {
         triggerButton = target;
-        searchModal();
+        triggeredElement = findModal();
+        if (triggeredElement.classList.contains(`by:${triggerButton.id}`)) {
+            show(triggeredElement);
+        }
+    }
+    triggeredElement = findModal();
+    target = e.target;
+    if (target.closest("[closing-attribute]")) {
+        hide(triggeredElement);
     }
 });
+function defaultModals(target) {
+    triggerButton = target;
+    triggeredElement = findModal();
+    let holder = findHolder(triggerButton);
+    if (holder) {
+        if (!holder.classList.contains("gen")) {
+            holder.classList.add("gen");
+            let html = generateModal(triggerButton, holder);
+            document.body.innerHTML += html;
+        }
+    }
+    if (triggeredElement.classList.contains(`by:${triggerButton.id}`)) {
+        show(triggeredElement);
+    }
+}
 export function getTrigger() {
     return triggerButton;
+}
+function findHolder(button) {
+    return Array.from(document.querySelectorAll(".holder")).find((holder) => holder.classList.contains(`by:${button.id}`));
 }
--- Public/javascripts/modals.js
+++ Public/javascripts/modals.js
@@ -1,41 +1,45 @@
-import { getTrigger, modals } from "./main.js";
-let animation;
-const animationDuration = 700;
+import { getTrigger } from "./main.js";
+const animationDuration = 400;
 let triggeredElement;
+let animation;
 export function searchModal() {
     const triggerButton = getTrigger();
-    modals.forEach((modal) => {
-        if (modal.classList.contains(`by:${triggerButton.id}`)) {
-            triggeredElement = modal;
-            animation = triggeredElement.dataset.animation;
-            show(modal);
-            return;
-        }
-    });
-    handleElement();
-}
-function handleElement() {
-    if (triggeredElement) {
-        triggeredElement.addEventListener("click", (e) => {
-            const target = e.target;
-            if (target.closest("[closing-attribute]")) {
-                hide(triggeredElement);
-            }
-        });
+    const modal = Array.from(document.querySelectorAll(".modal-container")).find((modal) => modal.classList.contains(`by:${triggerButton.id}`));
+    if (modal) {
+        triggeredElement = modal;
+        animation = triggeredElement.dataset.animation;
     }
+    return triggeredElement;
 }
-function show(modal) {
+export function show(modal) {
     if (isHidden(modal)) {
-        modal.classList.remove(animation);
+        modal.classList.remove(`do_${animation}`);
     }
     modal.classList.add(`show_${animation}`);
 }
-function hide(modal) {
-    modal.classList.add(animation);
+export function hide(modal) {
+    modal.classList.add(`do_${animation}`);
     setTimeout(() => {
         modal.classList.remove(`show_${animation}`);
     }, animationDuration);
 }
 function isHidden(modal) {
-    return modal.classList.contains("fade");
+    return modal.classList.contains(`do_${animation}`);
+}
+export function generateModal(trigger, holder) {
+    return `
+    <div data-animation ="${trigger.dataset.animation}" class="modal-container by:${trigger.id} show_${trigger.dataset.animation}">
+        <div class="modal-content">
+            <h1>${holder.querySelector(".title").textContent}</h1>
+            <p class="text">
+                ${holder.querySelector(".text").textContent} 
+            </p>
+            <div class="btn_grp">
+                <div class="grp_content">
+                    <button closing-attribute class="btn modal-danger-btn">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
 }
